b134974c8d7ef4c4aad108a595195a6fcc312bca
Piotrek Janisz <piotrekjanisz@gmail.com>
2014-09-05 07:33:28 -0400

    Add logging.

--- server_app/app/server.js
+++ server_app/app/server.js
@@ -1,12 +1,37 @@
 var mongoskin = require('mongoskin');
 var express = require('express');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 var app = express();
 
 var config = require('../config/application.js')[app.settings.env];
 
 var bodyparser = require('body-parser');
-app.use(bodyparser.json());
 
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ level: 'error' }),
+    new (winston.transports.File)({ level: 'info', filename: config.logging.file})
+  ]
+});
+
+app.use(bodyparser.json());
+app.use(expressWinston.logger({
+  transports: [
+    logger
+  ],
+  meta: true,
+  msg: "HTTP {{req.method}} {{req.url}}" 
+}));
+
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
 
 var db = mongoskin.db(config.db.mongodb, {safe:true});
 
@@ -30,7 +55,7 @@ app.get("/users/:username/tests", function(req, res, username) {
 function start(port) {
   port = port || 3000;
   app.listen(3000);
-  console.log("DIKY server listening on port %d in %s mode", port, app.settings.env);
+  logger.log("info", "DIKY server listening on port %d in %s mode", port, app.settings.env);
 }
 
 exports.start = start;
--- server_app/config/application.js
+++ server_app/config/application.js
@@ -2,11 +2,18 @@ module.exports = {
   "development": {
     "db": {
       "mongodb": "mongodb://@localhost:27017/diky"
+    },
+
+    "logging": {
+      "file": "logs/api.log"
+      "level": "error"
     }
   },
 
 
   "production": {
-
+    "logging": {
+      "level": "warn"
+    }
   }
 };
