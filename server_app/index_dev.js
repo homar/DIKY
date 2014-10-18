ac6a4289ab360a0bc690ff59f58bac8af1dbe356
piotr.janisz <piotr.janisz@ocado.com>
2014-10-18 11:43:21 +0200

    Fix index_dev.js

--- server_app/index_dev.js
+++ server_app/index_dev.js
@@ -1,13 +1,14 @@
 var server = require("./app/server.js");
 var mongoskin = require('mongoskin');
 
-var config = require('./config/application.js')['development'];
+var config = require('./config/application.js');
 var db = mongoskin.db(config.db.mongodb, {safe:true});
 
 var dev_data = require('./test/dev_test_data.js');
 
 db.collection('tests').insert(dev_data.tests, function(err, res) {   
 });
+
 db.collection('fulfilments').insert(dev_data.fulfilments, function(err, res) {
   server.start();
 });
